8f997976db09e38cba61fe011daecf803f739a2d
priesgo <priesgoferreiro@gmail.com>
2016-04-14 11:35:07 -0500

    changed annotation to just canonical and setting stats file names

--- annotation/annotation.sh
+++ annotation/annotation.sh
@@ -3,12 +3,20 @@ SNPEFF=/opt/snpEff/snpEff.jar
 SNPEFF_CONFIG=/opt/snpEff/snpEff.config
 
 INPUT_VCF=$1
-PREFIX=`basename $1`
+PREFIX_LOCAL=`basename $1`
 echo $INPUT_VCF
 OUTPUT_VCF=$2
 echo $OUTPUT_VCF
-REFERENCE=$3
-echo $REFERENCE
+SNPEFF_REFERENCE=$3
+echo $SNPEFF_REFERENCE
 
-java -jar $SNPEFF eff $REFERENCE $INPUT_VCF -c $SNPEFF_CONFIG > $OUTPUT_VCF
+# Translate NCBI identifiers into the sequence numbers used in SnpEff reference
+# matching was confirmed using the number of bp of each chromosome
+sed -i 's/NC_008510/1/g' $INPUT_VCF
+sed -i 's/NC_008511/2/g' $INPUT_VCF
+sed -i 's/NC_008508/1/g' $INPUT_VCF
+sed -i 's/NC_008509/2/g' $INPUT_VCF
+
+java -jar $SNPEFF eff $SNPEFF_REFERENCE $INPUT_VCF -c $SNPEFF_CONFIG -s $PREFIX_LOCAL.stats.html -canon > $OUTPUT_VCF
+mv snpEff_genes.txt $PREFIX_LOCAL.genes.stats.txt
 
