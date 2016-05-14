2b2272e01d1b5cfe8adbc1c77dd9acf03590eb9d
priesgo <priesgoferreiro@gmail.com>
2016-05-14 06:59:50 -0500

    removing hard filters only applicable to heterozygous calls

--- variant_filtering/variant_filtering.sh
+++ variant_filtering/variant_filtering.sh
@@ -11,14 +11,15 @@ echo $REFERENCE
 
 # Variant filtering pipeline for SNPs
 java -jar $GATK -T SelectVariants -R $REFERENCE -V $INPUT_VCF -selectType SNP -o $PREFIX_LOCAL.raw_snps.vcf
-java -jar $GATK -T VariantFiltration -R $REFERENCE -V $PREFIX_LOCAL.raw_snps.vcf --filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filterName "FP" -o $PREFIX_LOCAL.filtered_snps.vcf
+java -jar $GATK -T VariantFiltration -R $REFERENCE -V $PREFIX_LOCAL.raw_snps.vcf --filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0" --filterName "FP" -o $PREFIX_LOCAL.filtered_snps.vcf
 
 # Variant filtering pipeline for indels
 java -jar $GATK -T SelectVariants -R $REFERENCE -V $INPUT_VCF -selectType INDEL -o $PREFIX_LOCAL.raw_indels.vcf
-java -jar $GATK -T VariantFiltration -R $REFERENCE -V $PREFIX_LOCAL.raw_indels.vcf --filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" --filterName "FP" -o $PREFIX_LOCAL.filtered_indels.vcf
+java -jar $GATK -T VariantFiltration -R $REFERENCE -V $PREFIX_LOCAL.raw_indels.vcf --filterExpression "QD < 2.0 || FS > 200.0" --filterName "FP" -o $PREFIX_LOCAL.filtered_indels.vcf
 
-# Combine all variants
-java -jar $GATK -T CombineVariants -R $REFERENCE -o $OUTPUT_VCF -V $PREFIX_LOCAL.filtered_snps.vcf -V $PREFIX_LOCAL.filtered_indels.vcf --filteredAreUncalled --genotypemergeoption UNSORTED
+# Combine all variants 
+# --filteredAreUncalled
+java -jar $GATK -T CombineVariants -R $REFERENCE -o $OUTPUT_VCF -V $PREFIX_LOCAL.filtered_snps.vcf -V $PREFIX_LOCAL.filtered_indels.vcf --genotypemergeoption UNSORTED
 
 rm -f $PREFIX_LOCAL.filtered_snps.vcf
 rm -f $PREFIX_LOCAL.filtered_snps.vcf.idx
