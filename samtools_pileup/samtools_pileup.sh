5fb1ccf5e50d2c9f679842be7c1756082b260f44
priesgo <priesgoferreiro@gmail.com>
2016-05-18 23:01:28 -0500

    samtools variant calling for haploid organisms

--- samtools_pileup/samtools_pileup.sh
+++ samtools_pileup/samtools_pileup.sh
@@ -23,14 +23,7 @@ echo $REFERENCE
 
 
 # Variant calling with samtools pileup
-$SAMTOOLS_HOME/samtools mpileup -DSugBf $REFERENCE $INPUT_BAM > $PREFIX_LOCAL.tmp.bcf
-$BCFTOOLS_HOME/bcftools view -bvcg $PREFIX_LOCAL.tmp.bcf > $PREFIX_LOCAL.bcf
-$BCFTOOLS_HOME/bcftools index $PREFIX_LOCAL.bcf
-$BCFTOOLS_HOME/bcftools view $PREFIX_LOCAL.bcf | $VCFUTILS_HOME/vcfutils.pl varFilter - > $PREFIX_LOCAL.vcf
-
-rm -f $PREFIX_LOCAL.tmp.bcf
-rm -f $PREFIX_LOCAL.bcf
-
-java -jar $GATK -T VariantAnnotator -I $INPUT_BAM -R $REFERENCE -V $PREFIX_LOCAL.vcf -o $OUTPUT_VCF --annotation BaseQualityRankSumTest --annotation ClippingRankSumTest --annotation Coverage --annotation FisherStrand --annotation GCContent --annotation HomopolymerRun --annotation LikelihoodRankSumTest --annotation MappingQualityRankSumTest --annotation NBaseCount --annotation QualByDepth --annotation RMSMappingQuality --annotation ReadPosRankSumTest --annotation StrandOddsRatio --annotation TandemRepeatAnnotator --annotation DepthPerAlleleBySample --annotation DepthPerSampleHC --annotation StrandAlleleCountsBySample --annotation StrandBiasBySample --excludeAnnotation HaplotypeScore --excludeAnnotation InbreedingCoeff
-
+$SAMTOOLS_HOME/samtools mpileup -6EOsuf $REFERENCE $INPUT_BAM | $BCFTOOLS_HOME/bcftools call -mv -Ov --ploidy 1 > $PREFIX_LOCAL.vcf
+java -jar $GATK -T VariantAnnotator -I $INPUT_BAM -R $REFERENCE -V $PREFIX_LOCAL.vcf -o $OUTPUT_VCF --annotation BaseQualityRankSumTest --annotation ClippingRankSumTest --annotation Coverage --annotation FisherStrand --annotation GCContent --annotation HomopolymerRun --annotation LikelihoodRankSumTest --annotation NBaseCount --annotation QualByDepth --annotation RMSMappingQuality --annotation StrandOddsRatio --annotation TandemRepeatAnnotator --annotation DepthPerAlleleBySample --annotation DepthPerSampleHC --annotation StrandAlleleCountsBySample --annotation StrandBiasBySample --excludeAnnotation HaplotypeScore --excludeAnnotation InbreedingCoeff
+rm -f $PREFIX_LOCAL.vcf
 rm -f $PREFIX_LOCAL.vcf
