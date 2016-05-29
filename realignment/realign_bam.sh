3caad6aa3536c095d0ee9d879a81b08e597143c1
priesgo <priesgoferreiro@gmail.com>
2016-05-29 11:30:21 -0500

    rename realignment intervals file

--- realignment/realign_bam.sh
+++ realignment/realign_bam.sh
@@ -26,8 +26,8 @@ OUTPUT_DIR=$(dirname "$OUTPUT_BAM")
 
 # Preprocessing pipeline
 echo "GATK RealignerTargetCreator"
-java -Xmx1g -jar $GATK -T RealignerTargetCreator -R $REFERENCE -I $INPUT_BAM -o $OUTPUT_DIR/$PREFIX_LOCAL.realignment_intervals --filter_mismatching_base_and_quals
+java -Xmx1g -jar $GATK -T RealignerTargetCreator -R $REFERENCE -I $INPUT_BAM -o $OUTPUT_DIR/$PREFIX_LOCAL.intervals --filter_mismatching_base_and_quals
 
 echo "GATK IndelRealigner"  
-java -Xmx4g -Djava.io.tmpdir=. -jar $GATK -T IndelRealigner -I $INPUT_BAM -R $REFERENCE -targetIntervals $OUTPUT_DIR/$PREFIX_LOCAL.realignment_intervals -o $OUTPUT_BAM --filter_mismatching_base_and_quals --consensusDeterminationModel USE_READS
+java -Xmx4g -Djava.io.tmpdir=. -jar $GATK -T IndelRealigner -I $INPUT_BAM -R $REFERENCE -targetIntervals $OUTPUT_DIR/$PREFIX_LOCAL.intervals -o $OUTPUT_BAM --filter_mismatching_base_and_quals --consensusDeterminationModel USE_READS
 
