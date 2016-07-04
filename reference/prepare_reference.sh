334f1e48c67775b5a53044a019f7d8dc645fd3c0
Alejandro Llanes <thyngum@gmail.com>
2016-07-04 17:25:42 -0500

    Updates reference/prepare_reference.sh

--- reference/prepare_reference.sh
+++ reference/prepare_reference.sh
@@ -14,13 +14,19 @@ source $BASEDIR/../config/config.sh
 
 # Copies input BAM into local folder
 INPUT_REFERENCE=$1
-echo $INPUT_REFERENCE
+# echo $INPUT_REFERENCE
 PREFIX_LOCAL=`basename $1 .fasta`
 OUTPUT_DIR=$(dirname "$INPUT_REFERENCE")
 
+echo "Preparing the reference ..."
+echo
+
 # FAI index
+echo "$SAMTOOLS_HOME/samtools faidx $INPUT_REFERENCE"
 $SAMTOOLS_HOME/samtools faidx $INPUT_REFERENCE
 
 # Picard's dict index
+echo "java -jar $PICARD CreateSequenceDictionary R=$INPUT_REFERENCE O=${OUTPUT_DIR}/${PREFIX_LOCAL}.dict"
 java -jar $PICARD CreateSequenceDictionary R=$INPUT_REFERENCE O=${OUTPUT_DIR}/${PREFIX_LOCAL}.dict
 
+echo
